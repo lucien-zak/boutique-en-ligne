30b8fa2a0912c609ad4705ce662bd194a7aaff5a
sataf <bassem.sataf@laplateforme.io>
2022-02-07 16:05:17 +0100

    suppression class Model

--- App/Config/Database.php
+++ App/Config/Database.php
@@ -8,7 +8,7 @@ class Database
     // 
     protected $pdo;
 
-    public function connect() {
+    protected function connect() {
         $opt  = array(
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
@@ -16,6 +16,18 @@ class Database
         );
         $dsn = 'mysql:host=51.38.48.43;dbname=s37_boutique;';
         return $this->pdo = new PDO($dsn, 'u37_AQzitrHR8s', 'kj3++kaHeUZgukj8e^MLxc@T', $opt);
+    }
+
+    //Generic function:
+
+    public function find($id)
+    {
+        $stmt =  $this->connect()->prepare("SELECT * FROM users WHERE id = ?");
+        $stmt->execute(array($id));
+        $test = $stmt->fetchAll();
+
+        return $test;
+
     }
 
 }
