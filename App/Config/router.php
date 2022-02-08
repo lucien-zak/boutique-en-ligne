6e02a23ee643c0c2e5aec4520191ad8b0406a70c
Kilian Guerin <73956105+kilian-guerin@users.noreply.github.com>
2022-02-08 12:05:34 +0100

    supprime doublon de code

--- App/Config/router.php
+++ App/Config/router.php
@@ -55,16 +55,5 @@ if( is_array($match) && is_callable( $match['target'] ) ) {
     Shop::error();
 }
 
-// match current request url
-$match = $router->match();
 
-// call closure or throw 404 status
-if( is_array($match) && is_callable( $match['target'] ) ) {
-	call_user_func_array( $match['target'], $match['params'] ); 
-} else {
-	// no route was matched
-	echo '404';
-
-	header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
-}
 ?>
