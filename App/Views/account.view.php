287d673efc10b668d7e6d206fb7ecb7bd0299829
Kilian Guerin <73956105+kilian-guerin@users.noreply.github.com>
2022-02-09 12:44:27 +0100

    Creating css for user panel.

--- App/Views/account.view.php
+++ App/Views/account.view.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,12 +9,14 @@
     <link rel="stylesheet" href="./assets/style/account.view.css">
     <link rel="stylesheet" href="./assets/style/header.style.css">
     <link rel="stylesheet" href="./assets/style/normalize.css">
-<script src="https://kit.fontawesome.com/225d5fd287.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/225d5fd287.js" crossorigin="anonymous"></script>
 </head>
+
 <body>
     <main>
         <section class="home">
             <div class="container">
+                <?php if( !$_SESSION ) { ?>
                 <img src="./assets/img/icons/logo-enterprise-1.png" alt="">
                 <div class="buttons-container">
                     <div class="button">
@@ -27,8 +30,24 @@
                         </a>
                     </div>
                 </div>
+                <?php } else { ?>
+                <img class="logo-user" src="./assets/img/icons/user-default.png" alt="">
+                <h2 class="user-title">Bienvenue Kilian</h2>
+                <div class="buttons-container">
+                    <form action="/account/profil" method="POST">
+                        <div class="buttons-container">
+                            <button class="button" type="submit">Mes Informations</button>
+                            <button class="button" type="submit">Mes Adresses</button>
+                            <button class="button" type="submit">Moyen de paiements</button>
+                            <button class="button" type="submit">Mes Commandes</button>
+                            <button class="button" type="submit">Se déconnecter</button>
+                        </div>
+                    </form>
+                </div>
+                <?php } ?>
             </div>
         </section>
     </main>
 </body>
+
 </html>
