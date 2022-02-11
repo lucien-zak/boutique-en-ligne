90e6525157b5a04b1c5bd70402dfd5f045f1a809
Kilian Guerin <73956105+kilian-guerin@users.noreply.github.com>
2022-02-11 10:31:59 +0100

    Fix css header

--- App/Views/header.view.php
+++ App/Views/header.view.php
@@ -1,12 +1,8 @@
-<?php 
-// session_start() 
-?>
-
 <header>
     <nav>
-        <a href="/"><i class="fas fa-home"></i></a>
-        <a href="/products"><i class="fas fa-record-vinyl"></i></a>
-        <a href="/cart"><i class="fas fa-shopping-cart"></i></a>
-        <a href="/account"><i class="fas fa-user"></i></a>
+        <a <?= $params['titre'] == "Accueil" ? "class='color-orange'" : "";?> href="/"><i class="fas fa-home"></i></a>
+        <a <?= $params['titre'] == "Accueil" ? "class='color-orange'" : "";?> href="/products"><i class="color fas fa-record-vinyl"></i></a>
+        <a <?= $params['titre'] == "Accueil" ? "class='color-orange'" : "";?> href="/cart"><i class="color fas fa-shopping-cart"></i></a>
+        <a <?= $params['titre'] == "Account" || "Profil" || "Vos Adresses" || "Vos Paiements" ? "class='color-orange'" : "";?> href="/account"><i class="color fas fa-user"></i></a>
     </nav>
 </header>
