622316aac5f71dc600bda274c7632f9657d2f4d0
Kilian Guerin <73956105+kilian-guerin@users.noreply.github.com>
2022-02-14 15:35:46 +0100

    Add condition on address of user

--- App/Views/account.address.view.php
+++ App/Views/account.address.view.php
@@ -14,7 +14,8 @@
     <main>
         <section class="home">
             <!-- Faire une condition pour savoir si une carte à déjà été ajouté. -->
-            <!-- <div class="container">
+            <?php if($params['nb'] < 1) { ?>
+            <div class="container">
                 <a class="box-add" href="/account/addresses/add">
                     <span class="box-icon fa-stack">
                         <i class="icon far fa-circle fa-stack-2x"></i>
@@ -22,41 +23,45 @@
                     </span>
                     <h3>AJOUTER UNE ADRESSE</h3>
                 </a>
-            </div> -->
+            </div>
+            <?php } else { ?>
             <div class="container customer">
             <div class="container-top">
                 <div class="box-top">
                     <h3>VOS ADRESSES</h3>
+                    <?php if($params['nb'] < 2) { ?>
                     <a href="/account/addresses/add">
                         <i class="icon fas fa-plus"></i>
                     </a>
+                    <?php } ?>
                 </div>
                 <form action="/account/profil" enctype="multipart/form-data" method="POST">
                     <div class="entry-container">
                          <?php
-                            // foreach($params['data'] as $adress)
-                            // {
-                            //     echo '
-                            //     <div class="box profil">
-                            //         <div class="left"></div>';
-                            //     echo "<h3>$adress[type]</h3>";  
-                            //     echo "<p>$adress[full_name]</p>";
-                            //     echo "<p>$adress[adress]</p>";
-                            //     if(isset($adress['additional_adress'])) {
-                            //         echo "<p>$adress[additional_adress]</p>";
-                            //     }
-                            //     echo "<p>$adress[postal_code] $adress[city]</p>";
-                            //     echo '
-                            //     <div class="buttons-container">
-                            //         <button class="button edit" type="submit">Modifier</button>
-                            //     </div>
-                            //         <div class="right"></div>
-                            //     </div>';
-                            // }
+                            foreach($params['data'] as $adress)
+                            {
+                                echo '
+                                <div class="box profil">
+                                    <div class="left"></div>';
+                                echo "<h3>$adress[type]</h3>";  
+                                echo "<p>$adress[full_name]</p>";
+                                echo "<p>$adress[adress]</p>";
+                                if(isset($adress['additional_adress'])) {
+                                    echo "<p>$adress[additional_adress]</p>";
+                                }
+                                echo "<p>$adress[postal_code] $adress[city]</p>";
+                                echo '
+                                <div class="buttons-container">
+                                    <button class="button edit" type="submit">Modifier</button>
+                                </div>
+                                    <div class="right"></div>
+                                </div>';
+                            }
                         ?> 
                     </div>
                 </form>
             </div>
+            <?php } ?>
         </section>
     </main>
 </body>
